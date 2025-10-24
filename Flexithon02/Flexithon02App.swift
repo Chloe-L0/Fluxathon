90c7492316caa34da7232df80b1645cbc5abe275
chloe <lochey2004@gmail.com>
2025-10-24 00:25:57 -0400

    Initial Commit

---
+++ Flexithon02/Flexithon02App.swift
@@ -0,0 +1,17 @@
+//
+//  Flexithon02App.swift
+//  Flexithon02
+//
+//  Created by chloe on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Flexithon02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
